886e8af21102fb682d2f7c86f59dee791a77f600
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-10-10 19:59:04 +0200

    Index.html updated

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+// Set the time when end is near :)
 const endIsNear = "1 Jan 2025";
+
+// Global variable  for Element in html5
+
+
+
+
 
 function countDown(){
     const endIsNearDate = new Date(endIsNear);
     const currentTimeDate = new Date();
-    console.log(endIsNearDate);
-    console.log(currentTimeDate);
+ 
+    // Total Seconds 
+    const totalSeconds = (endIsNearDate - currentTimeDate)/1000;
+
+
+    const days = Math.floor(totalSeconds/ 3600 / 24);
+    const hours = Math.floor(totalSeconds/ 3600 % 24);
+    const minutes = Math.floor(totalSeconds/ 60 % 60);
+    const seconds = Math.floor(totalSeconds % 60);
+
+    console.log(days,hours,minutes, seconds);
 }
+
+
 
 
 // initial call 
